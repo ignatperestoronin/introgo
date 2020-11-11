247bd48b88cba83024dccc1009ae7b7494175579
Пересторонин Игнат Сергеевич <71984448+ignatperestoronin@users.noreply.github.com>
2020-11-11 21:07:31 +0300

    Update rain.go

--- rain.go
+++ rain.go
@@ -1,22 +1,7 @@
 package main
 
-import (
-    "net/http"
-    "os"
-)
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("<h1>Hello World!</h1>"))
-}
+import "fmt"
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "3000"
-    }
-
-    mux := http.NewServeMux()
-
-    mux.HandleFunc("/", indexHandler)
-    http.ListenAndServe(":"+port, mux)
+    fmt.Println("Hello, my name is Ignat")
 }
