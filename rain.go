bc33be5360b5d271090e0f8aff072bc9aa46011d
Пересторонин Игнат Сергеевич <71984448+ignatperestoronin@users.noreply.github.com>
2020-11-23 20:02:06 +0300

    Update rain.go

--- rain.go
+++ rain.go
@@ -1,7 +1,43 @@
+# 1
+
 package main
 
 import "fmt"
 
 func main() {
     fmt.Println("Hello, my name is Ignat")
+}
+
+# 2
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("5 + 5 = ", 5 + 5)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("10 - 12 = ", 10 - 12)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("10 - 6.666 = ", 10 - 6.666)
+}
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("10.2 - 6.666 = ", 1 + 1)
 }
