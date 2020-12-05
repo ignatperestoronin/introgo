2be8da699d2d0f51dd32e48565ca4de5434791d3
unknown <perestoronin_i@mail.ru>
2020-12-05 22:16:09 +0300

    refactor

---
+++ types/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("5 + 5 = ", 5+5)
+	fmt.Println("10 - 12 = ", 10-12)
+	fmt.Println("10 - 6.666 = ", 10-6.666)
+	fmt.Println("10.2 - 6.666 = ", 1+1)
+}
